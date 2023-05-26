42374764d44fd2c6af739a0fcf0b35bb7e9766cf
沈赟杰 <shenyunjie@znrank.com>
2023-05-26 17:22:17 +0800

    update

--- packages/functional/src/component.ts
+++ packages/functional/src/component.ts
@@ -2,17 +2,30 @@ import { Hook } from './hook';
 import type { 
   Component, 
   ComponentChildren, 
-  ComponentResult, 
   ComponentWrapper,
+  ComponentRender,
   PureCompoenntProps,
 } from './types';
 
+/**
+ * create a new component render
+ * run it with hook
+ * it will return a value finnally
+ * @param callback running function component
+ * @param props function component props
+ * @param args children function component
+ * @returns 
+ */
 export function createComponent<T extends Component>(
   callback: T, 
   props?: PureCompoenntProps<T>, 
-  ...args: ComponentWrapper<ComponentChildren<T>>
-) {
-  return async (hook?: Hook): Promise<ComponentResult<T> extends Promise<infer H> ? H : ComponentResult<T>> => {
+  ...childrens: ComponentWrapper<ComponentChildren<T>>
+): ComponentRender<T> {
+  return async (hook?: Hook) => {
+    // define useHook function
+    // inject into props
+    // it collect fns from hook provider
+    // then run the each hooks at the end of fn result
     const useHook = async <R>(key: string, fn: () => R) => {
       const res = await Promise.resolve(fn());
       if (hook && hook.has(callback)) {
@@ -21,13 +34,20 @@ export function createComponent<T extends Component>(
       return res;
     }
 
-    const children = await Promise.all(args.map(child => {
+    // run the children state
+    // before callback
+    const children = await Promise.all(childrens.map(child => {
+      // support common values
       if (typeof child !== 'function') return Promise.resolve(child);
+      // support component render
       return Promise.resolve(child(hook));
     }));
 
+    // run the callback fn
     const res = await Promise.resolve(
       callback(
+        // freeze the object
+        // make it unresetable
         Object.freeze(
           Object.assign(
             props || {}, 
@@ -39,6 +59,13 @@ export function createComponent<T extends Component>(
         )
       )
     );
+
+    // if return a function
+    // that means it is a component
+    // so we should run the component with hook
+    if (typeof res === 'function') {
+      return await Promise.resolve(res(hook));
+    }
     
     return res;
   }
--- packages/functional/src/types.ts
+++ packages/functional/src/types.ts
@@ -3,9 +3,25 @@ export type Props<I, C extends Array<any> = []> = Readonly<I & {
   children: C,
   useHook: <R>(key: string, fn: () => R) => Promise<R> 
 }>;
-export type Component<I = {}, C extends Array<any> = any[], O = any> = (props?: Props<I, C>) => O;
+
+export type Component<I = {}, C extends Array<any> = any[], O = any> = (props?: Props<I, C>) => O | Promise<O> | ((hook?: Hook) => Promise<O>);
+
 export type ComponentChildren<T> = T extends (...args: any[]) => any ? Parameters<T>[0] extends { readonly children: infer C } ? C : [] : [];
+
+export type ComponentWrapper<T extends any[]> = [
+  ...{ 
+    [K in keyof T]: ((hook?: Hook) => Promise<T[K]>) | T[K]
+  }
+];
+
 export type ComponentProps<T> = T extends Component<infer I, infer C, any> ? Props<I, C> : never;
-export type ComponentResult<T> = T extends Component<any, any[], infer O> ? O : never;
-export type ComponentWrapper<T extends any[]> = [...{ [K in keyof T]: ((hook?: Hook) => Promise<T[K]>) | T[K] }];
+
 export type PureCompoenntProps<T> = ComponentProps<T> extends Props<infer P, any> ? Omit<P, 'children' | 'useHook'> : never;
+
+export type ComponentRender<T extends (...args: any[]) => any> = (hook?: Hook) => Promise<
+  ReturnType<T> extends ComponentRender<infer N> 
+    ? ReturnType<N> extends Promise<infer P>
+      ? P
+      : ReturnType<N>
+    : ReturnType<T>
+>;
