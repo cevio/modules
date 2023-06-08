980a9a41902315c8258a2a5154cacc55dfeb911a
沈赟杰 <shenyunjie@znrank.com>
2023-06-08 19:38:25 +0800

    update

--- packages/define/src/hook.ts
+++ packages/define/src/hook.ts
@@ -8,4 +8,30 @@ export class Hook extends Map<DefineFeedback<any, any>, Node> {
     }
     return this.get(fn);
   }
+
+  static merge(...hooks: Hook[]) {
+    const hook = new Hook();
+    let i = hooks.length;
+
+    while (i--) {
+      const current = hooks[i];
+      for (const [feedback, node] of current.entries()) {
+        if (!hook.has(feedback)) {
+          hook.set(feedback, new Node());
+        }
+        const hookNode = hook.get(feedback);
+        for (const [name, stacks] of node) {
+          if (!hookNode.has(name)) {
+            hookNode.set(name, new Set());
+          }
+          const local = hookNode.get(name);
+          for (const stack of stacks.values()) {
+            local.add(stack);
+          }
+        }
+      }
+    }
+
+    return hook;
+  }
 }
